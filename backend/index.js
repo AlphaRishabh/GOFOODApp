3460e6c9f2886990a3cc3fc7ade1ef93520d1708
AlphaRishabh <rishabh.pandey751@gmail.com>
2024-04-13 02:48:32 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-
+const path = require("path");
 global.foodData = require('./db')(function call(err, data, CatData) {
   // console.log(data)
   if(err) console.log(err);
@@ -10,7 +10,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://mern-foodorder-app.azurewebsites.net");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -24,6 +24,11 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/auth', require('./Routes/Auth'));
+
+app.use(express.static("./build"))  ;
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
